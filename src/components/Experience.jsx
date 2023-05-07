ca7a87a5b5f1afea3a6d057363b5278dfaffce4f
Prajeet Basnet <prajitbasnet2000@gmail.com>
2023-05-07 11:53:10 +1000

    fixed key for list in experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -79,9 +79,9 @@ const Experience = () => {
 
                 {
                     techs.map(({id,src,title,style})=>
-                    <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                    <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
 
-                    <img key={id} src={src} alt="logos" className='w-20 mx-auto'/>
+                    <img src={src} alt="logos" className='w-20 mx-auto'/>
                     <p className='mt-4'>{title}</p>
                 </div>
                     )
