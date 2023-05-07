c0568a00b60ed510eb41a331f9d37e84109dc09b
Prajeet Basnet <prajitbasnet2000@gmail.com>
2023-05-07 11:34:25 +1000

    bug fixes in experience componenet

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -79,7 +79,7 @@ const Experience = () => {
 
                 {
                     techs.map(({id,src,title,style})=>
-                    <div className={"shadow-md hover:scale-105 duration-500 py-2 rounded-lg" + " " + style}>
+                    <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
 
                     <img key={id} src={src} alt="logos" className='w-20 mx-auto'/>
                     <p className='mt-4'>{title}</p>
